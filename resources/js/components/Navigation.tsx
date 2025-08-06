66a265107d2ad688c168a2b14c281c2c839befdb
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2025-08-06 16:58:23 +0200

    nav mobile

--- resources/js/components/Navigation.tsx
+++ resources/js/components/Navigation.tsx
@@ -4,7 +4,7 @@ export default function Navigation() {
     return (
             <div className="flex flex-col items-center bg-[#FDFDFC] p-6 text-[#1b1b18] lg:justify-center lg:p-8 dark:bg-[#0a0a0a]">
                 <header className="mb-6 w-full max-w-[335px] text-sm not-has-[nav]:hidden lg:max-w-4xl">
-                    <nav className="flex items-center justify-center gap-4">
+                    <nav className="flex items-center justify-center gap-4 flex-wrap">
                         <Link
                             href={route('calendar.index')}
                             className="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#20200a] hover:border-[#19140035]"
