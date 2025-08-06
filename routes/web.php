71902a5bb9fca80b96a2e98c5d4803cbed3c0a53
Kamil Wojtalak <kamilwojtalak99@gmail.com>
2025-08-06 21:33:34 +0200

    add dummy routes

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,41 @@ Route::middleware(['auth', 'verified'])->group(function () {
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
 
-Route::get('/kalendarz', function () {
+// NOTE can rename to  /panel in the future, and conditionally render based on user role, but it is PoC
+Route::get('/kierownik', function () {
     return Inertia::render('Calendar/Index');
-})->name('calendar.index');
+})->name('dashboard.manager.index');
 
+// NOTE can rename to  /panel in the future, and conditionally render based on user role, but it is PoC
 Route::get('/technik', function () {
     return Inertia::render('Technician/Index');
-})->name('technician.index');
+})->name('dashboard.technician.index');
+
+Route::get('/technik/zlecenie-serwisowe/szczegoly', function () {
+    return Inertia::render('Technician/Services/Show');
+})->name('dashboard.technician.services.show');
+
+Route::get('/technik/zlecenie-serwisowe/start', function () {
+    return Inertia::render('Technician/Services/Start');
+})->name('dashboard.technician.services.start');
 
 Route::get('/zlecenia-serwisowe/stworz', function () {
     return Inertia::render('Services/Create');
 })->name('services.create');
+
+Route::get('/zlecenia-serwisowe/{service}', function ($service) {
+    // No validation its PoC
+    return Inertia::render('Services/Show');
+})->name('services.show');
+
+Route::get('/technicy', function () {
+    return Inertia::render('ServiceWorkers/Index');
+})->name('service-workers.index');
+
+Route::get('/klienci', function () {
+    return Inertia::render('Clients/Index');
+})->name('clients.index');
+
+Route::get('/maszyny', function () {
+    return Inertia::render('Machines/Index');
+})->name('machines.index');
